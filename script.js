bea54865b5d54490cfbe71e1248f184fc54d1c91
Winnie Phan <nhuphan0404@gmail.com>
2018-04-24 14:25:35 +0700

    edit file name

---
+++ script.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 10) {
+        $(".header").addClass("active");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $(".header").removeClass("active");
+    }
+  });
+});
+
+
+
--- spotify.js
+++
@@ -1,9 +0,0 @@
-let wrap = $("#wrap"); 
-wrap.on("scroll", function(e) {
-  wrap.preventDefault(e);
-  if(this.scrollTop>2) {
-    wrap.addClass("fix-header");
-  } else {
-    wrap.removeClass("fix-header");
-  }
-})
